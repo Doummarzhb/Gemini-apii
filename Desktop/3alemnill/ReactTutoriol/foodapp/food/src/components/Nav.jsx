c503a81f240d1957a1d5c46408675bbc0bdcf4ef
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2024-08-21 21:33:23 +0300

    Delete Desktop/3alemnill/ReactTutoriol/foodapp/food directory

--- Desktop/3alemnill/ReactTutoriol/foodapp/food/src/components/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './nav.module.css';
-export default function Nav(){
-    return (
-        <div className={styles.nav}>
-            Food App 
-        </div>
-    )
-}
